70dc7d98e57e1323745645abe337a91357f85811
Harshavardhan Kulkarni <55760311+Harshavardhan341@users.noreply.github.com>
2021-11-22 09:28:29 +0530

    week4

---
+++ Tutorial4/tut4_q7.m
@@ -0,0 +1,23 @@
+clear; clc; close all;
+h=fir1(82,[0.3624 0.5437],blackman(83));
+freqz(h,1,512)
+figure
+% mod5_7b
+f = [0 5 8 12 15 22.07]/22.07;
+m = [0 0 1 1 0 0];
+b = fir2(80,f,m);
+[h,w] = freqz(b,1,128);
+plot(f,m,w/pi,abs(h));
+legend('ldeal','fir2 Designed')
+title('Comparison of Frequency Response Magnitudes');
+figure
+% mod5_7c
+rp = 0.0015; % Passband ripple
+rs = 0.001; % Stopband ripple
+fs = 44140; % Sampling frequency
+f = [5000 8000 12000 15000]; % Cutoff frequencies
+a = [0 1 0 ]; % Desired amplitudes
+dev=[rs rp rs]; % deviations
+[n, fo, mo, w]=remezord(f, a, dev, fs);
+hopt = remez(n,fo,mo,w);
+[H ,W]=freqz(hopt,1,1024);
