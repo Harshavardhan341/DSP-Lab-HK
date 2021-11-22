8afd40ca6ffb7d71f54ae990251a2672c1a2712d
Harshavardhan Kulkarni <55760311+Harshavardhan341@users.noreply.github.com>
2021-11-22 09:59:37 +0530

    Delete Tutorial4 directory

--- Tutorial4/tut4_q6.m
+++
@@ -1,8 +0,0 @@
-% FIR filter design using Remez Exchange method
-n = 20; % length of filter
-f = [0 0.4 0.5 1]; % filter specs
-m = [1 1 0 0];
-bfir = remez(n,f,m)
-[hfir,wfir] = freqz(bfir);
-plot(f,m,wfir/pi,abs(hfir),'-');
-title(' n=20 FIR LPF');
